8761c1ca212c021ab60d1e6f7a3b2422e33ecd2a
Łukasz Strzelecki <lukasz.strz91@gmail.com>
2024-05-28 01:01:57 +0200

    draft in progress

---
+++ RaftMQ/NodeState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RaftMQ
+{
+    public enum NodeState
+    {
+        FOLLOWER,
+        CANDIDATE,
+        LEADER
+    }
+}
